0ed8a4f726d9dfb5f382c82ea77165777e22106f
Gwerh <gwerh.dev@gmail.com>
2024-07-08 17:41:11 -0400

    styling components

--- src/helpers/LocalStorage.functions.ts
+++ src/helpers/LocalStorage.functions.ts
@@ -1,5 +1,5 @@
-export const getUserToken = () => localStorage.getItem('userToken');
-export const setUserToken = (token: string) => localStorage.setItem('userToken', token);
+export const getUserToken = () => localStorage ? localStorage.getItem('userToken') : "";
+export const setUserToken = (token: string) => localStorage ? localStorage.setItem('userToken', token) : "";
 
 export const options = () => {
   return { 
