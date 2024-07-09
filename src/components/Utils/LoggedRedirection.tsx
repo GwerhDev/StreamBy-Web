0ce35ed35b04ec5b60069eda067467d7c1d36109
Gwerh <gwerh.dev@gmail.com>
2024-07-09 08:35:56 -0400

    styling components

--- src/components/Utils/LoggedRedirection.tsx
+++ src/components/Utils/LoggedRedirection.tsx
@@ -1,16 +1,5 @@
-"use client"
 
-import { getUserToken } from "@/helpers/LocalStorage.functions";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
 export const LoggedRedirection = () => {
-  const router = useRouter();
-  const token = getUserToken();
-  
-  useEffect(() => {
-    token ? router.push('/dashboard') : null;
-  }, [token]);
-
   return <></>;
 };
