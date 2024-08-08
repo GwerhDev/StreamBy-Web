0922e105e2a485d7044d070cde2940da837f67f8
Gwerh <gwerh.dev@gmail.com>
2024-08-08 11:38:43 -0400

    connecting with mongodb as serverless

--- src/app/api/integrations/mongodb.ts
+++ src/app/api/integrations/mongodb.ts
@@ -1,14 +1,8 @@
 import mongoose from "mongoose";
 import { mongodbString } from "../config";
 
-mongoose.set('strictQuery', false);
-
 const connectDB = async () => {
-  try {
-    return await mongoose.connect(mongodbString || "");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-  }
+ await mongoose.connect(mongodbString || "");
 };
 
 export default connectDB;
