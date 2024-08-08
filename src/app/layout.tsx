4e0b9a80ba28a755f988ec7503c473920e8d741c
Gwerh <gwerh.dev@gmail.com>
2024-08-08 16:46:43 -0400

    changing branding

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,20 +12,14 @@ export const metadata = {
   description: 'Storage & Streaming services',
 };
 
-export default async function RootLayout({ children }: { children: React.ReactNode }) {
-  try {
-
-  } catch (error) {
-    console.error(error);
-  } finally {
-    return (
-      <html lang="en">
-        <body className={inter.className}>
-          <Navigator />
-          {children}
-          <UnderConstruction />
-        </body>
-      </html>
-    );
-  };
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navigator />
+        {children}
+        <UnderConstruction />
+      </body>
+    </html>
+  );
 };
