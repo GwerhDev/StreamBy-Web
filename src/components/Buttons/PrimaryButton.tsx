e03039c7f4d32d9e6caa816ea6999e4301b13d7c
Gwerh <gwerh.dev@gmail.com>
2024-08-13 11:58:59 -0400

    styling buttons

--- src/components/Buttons/PrimaryButton.tsx
+++ src/components/Buttons/PrimaryButton.tsx
@@ -1,14 +1,14 @@
 import s from './PrimaryButton.module.css';
 
 export const PrimaryButton = (props: any) => {
-  const { text, action } = props;
+  const { text, action, type } = props;
 
   function handleClick() {
     action();
   }
 
   return (
-    <button className={s.container} onClick={handleClick}>
+    <button className={s.container} onClick={handleClick} type={type || "button"} >
       {text}
     </button>
   )
